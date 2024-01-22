b6aafc6553c06727cdd505350b4b85e0ededf9b8
Ruoyao Shi <yuxiaow@umich.edu>
2024-01-22 14:54:57 -0500

    Update extract traits

--- R/extract_traits.R
+++ R/extract_traits.R
@@ -9,6 +9,7 @@
 #' @param kb clumping kb window. Default=10000
 #' @param access_token Google OAuth2 access token. Default=check_access_token()
 #' @param min_snps the number of minimum shared SNPs with IV of X. Default=5
+#' @param min_instruments the number of minimum instruments for each selected traits. Default=3
 #' @returns A GWAS ID vector and a trait info dataframe
 #'
 #' @import ieugwasr
@@ -16,7 +17,8 @@
 #' @export
 extract_traits<-function(id_exposure,id_outcome,batch = c("ieu-a","ieu-b","ukb-b"),
                          pop = "EUR",pval_x=5e-8, pval_z=1e-5,r2=0.001,kb = 10000,
-                         access_token = ieugwasr::check_access_token(),min_snps=5){
+                         access_token = ieugwasr::check_access_token(),min_snps=5,
+                         min_instruments = 3){
   batch1 <- c("ieu-a","ieu-b","ukb-b")
   batch2 <- batch[!batch %in% batch1]
   phe1 <- retrieve_traits(id_exposure, pval_x,pval_z,pop = pop, batch=batch1,
--- pipeline/R/1_extract_traits.R
+++ pipeline/R/1_extract_traits.R
@@ -12,10 +12,11 @@ pval_z <- as.numeric(snakemake@params[["pval_traits"]])
 r2 <- as.numeric(snakemake@params[["r2_thresh"]])
 kb <- as.numeric(snakemake@params[["clump_kb"]])
 min_snps <- as.numeric(snakemake@params[["min_snps"]])
+min_instruments <- as.numeric(snakemake@params[["min_instruments"]])
 out <- snakemake@output[["out"]]
 
 res <- extract_traits(id_exposure = id_exposure, id_outcome = id_outcome,
-               batch = batch, pop = pop, pval_x = pval_x,
-               pval_z = pval_z, r2 = r2, kb = kb, min_snps = min_snps)
+                      batch = batch, pop = pop, pval_x = pval_x,
+                      pval_z = pval_z, r2 = r2, kb = kb, min_snps = min_snps,
+                      min_instruments = min_instruments)
 saveRDS(res,file = out)
-
