a6ea97f4cc4f82f392e8a9761222e656ef8da78f
Ruoyao Shi <yuxiaow@umich.edu>
2024-02-20 10:03:04 -0500

    Update quality_control.R

--- R/quality_control.R
+++ R/quality_control.R
@@ -14,16 +14,19 @@ quality_control <- function(dat,nsnp_cutoff=1e6,pop="European",gender="Males and
   id.list2 <- dat %>% filter(is.na(nsnp) & sex == gender & population == pop) %>% pull(id)
   id.list3 <- dat %>% filter(is.na(sex) | sex == "NA" & nsnp > nsnp_cutoff & population == pop) %>% pull(id)
   id.list <- unique(c(id.list1,id.list2,id.list3))
-  dat <- dat %>%
-    dplyr::mutate(status = if_else(id %in% id.list, "select after QC", "delete in QC"))
+  id_exposure <- dat %>% filter(status == "Delete due to it's the main exposure") %>% pull(id)
+  id.list <- id.list[!id.list %in% id_exposure]
+  dat <- dat %>% mutate(status = case_when(id %in% id.list ~ "select after QC",
+                                           id %in% id_exposure  ~ "Delete due to it's the main exposure",
+                                           TRUE ~ "delete in QC"))
   # delete menarche trait with sex males and females
   dat <- dat %>%
-    dplyr::mutate(status = ifelse(grepl('menarche', trait) == TRUE & sex == "Males and Females",
+    mutate(status = ifelse(grepl('menarche', trait) == TRUE & sex == "Males and Females",
                            "delete in QC",status))
   # delete trait-adjusted traits
   dat <- dat %>%
-    dplyr::mutate(status = ifelse(grepl('adjust', trait) == TRUE,
+    mutate(status = ifelse(grepl('adjust', trait) == TRUE,
                            "delete in QC",status))
-  id.qc <- dat %>% dplyr::filter(status == "select after QC") %>% pull(id)
+  id.qc <- dat %>% filter(status == "select after QC") %>% pull(id)
   return(list(id.list=id.qc,trait.info=dat))
 }
