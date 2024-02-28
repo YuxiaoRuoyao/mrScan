4ae5e6886e9cd17fc98989025ccf5b1341b63b4e
Ruoyao Shi <yuxiaow@umich.edu>
2024-02-28 18:13:11 -0500

    Update bidirection_mvmr.R

--- R/bidirection_mvmr.R
+++ R/bidirection_mvmr.R
@@ -38,29 +38,45 @@ bidirection_mvmr <- function(ex_dat1,ex_dat2,ex_dat3,ex_dat4,min_instruments = 3
     mvdat_1 <- mv_harmonise_data(ex_dat1,out_1_2)
     mvdat_2 <- mv_harmonise_data(ex_dat3,out_3_4)
 
-    grapple.dat1<-cbind(mvdat_1$exposure_beta,mvdat_1$exposure_se,
-                        mvdat_1$outcome_beta,mvdat_1$outcome_se)
-    grapple.dat2<-cbind(mvdat_2$exposure_beta,mvdat_2$exposure_se,
-                        mvdat_2$outcome_beta,mvdat_2$outcome_se)
-    i <- ncol(mvdat_1$exposure_beta)
-    j <- ncol(mvdat_2$exposure_beta)
-    colnames(grapple.dat1)<-c(paste0("gamma_exp",seq(1,i)),
-                              paste0("se_exp",seq(1,i)),"gamma_out1","se_out1")
-    colnames(grapple.dat2)<-c(paste0("gamma_exp",seq(1,j)),
-                              paste0("se_exp",seq(1,j)),"gamma_out1","se_out1")
-    grapple.res1<-grappleRobustEst(grapple.dat1,plot.it =FALSE,niter = 100000)
-    grapple.res2<-grappleRobustEst(grapple.dat2,plot.it =FALSE,niter = 100000)
+    # grapple.dat1<-cbind(mvdat_1$exposure_beta,mvdat_1$exposure_se,
+    #                     mvdat_1$outcome_beta,mvdat_1$outcome_se)
+    # grapple.dat2<-cbind(mvdat_2$exposure_beta,mvdat_2$exposure_se,
+    #                     mvdat_2$outcome_beta,mvdat_2$outcome_se)
+    # i <- ncol(mvdat_1$exposure_beta)
+    # j <- ncol(mvdat_2$exposure_beta)
+    # colnames(grapple.dat1)<-c(paste0("gamma_exp",seq(1,i)),
+    #                           paste0("se_exp",seq(1,i)),"gamma_out1","se_out1")
+    # colnames(grapple.dat2)<-c(paste0("gamma_exp",seq(1,j)),
+    #                           paste0("se_exp",seq(1,j)),"gamma_out1","se_out1")
+    # grapple.res1<-grappleRobustEst(grapple.dat1,plot.it =FALSE,niter = 100000)
+    # grapple.res2<-grappleRobustEst(grapple.dat2,plot.it =FALSE,niter = 100000)
+    # res1 <- data.frame(id.exposure=colnames(mvdat_1$exposure_beta),
+    #                    id.outcome=mvdat_1$outname$id.outcome,
+    #                    b=grapple.res1$beta.hat,
+    #                    se=sqrt(diag(grapple.res1$beta.var)),
+    #                    pval=grapple.res1$beta.p.value) %>%
+    #   filter(id.exposure == ID1)
+    # res2 <- data.frame(id.exposure=colnames(mvdat_2$exposure_beta),
+    #                    id.outcome=mvdat_2$outname$id.outcome,
+    #                    b=grapple.res2$beta.hat,
+    #                    se=sqrt(diag(grapple.res2$beta.var)),
+    #                    pval=grapple.res2$beta.p.value) %>%
+    #   filter(id.exposure == ID2)
+    fit1 <- esmr(beta_hat_Y = mvdat_1$outcome_beta, se_Y = mvdat_1$outcome_se,
+                 beta_hat_X = mvdat_1$exposure_beta,se_X = mvdat_1$exposure_se)
+    fit2 <- esmr(beta_hat_Y = mvdat_2$outcome_beta, se_Y = mvdat_2$outcome_se,
+                beta_hat_X = mvdat_2$exposure_beta,se_X = mvdat_2$exposure_se)
     res1 <- data.frame(id.exposure=colnames(mvdat_1$exposure_beta),
                        id.outcome=mvdat_1$outname$id.outcome,
-                       b=grapple.res1$beta.hat,
-                       se=sqrt(diag(grapple.res1$beta.var)),
-                       pval=grapple.res1$beta.p.value) %>%
+                       b=fit1$beta$beta_m,
+                       se = fit1$beta$beta_s) %>%
+      mutate(pval = 2*pnorm(-abs(b/se))) %>%
       filter(id.exposure == ID1)
     res2 <- data.frame(id.exposure=colnames(mvdat_2$exposure_beta),
                        id.outcome=mvdat_2$outname$id.outcome,
-                       b=grapple.res2$beta.hat,
-                       se=sqrt(diag(grapple.res2$beta.var)),
-                       pval=grapple.res2$beta.p.value) %>%
+                       b=fit2$beta$beta_m,
+                       se = fit2$beta$beta_s) %>%
+      mutate(pval = 2*pnorm(-abs(b/se))) %>%
       filter(id.exposure == ID2)
     return(list(mr12 = res1, mr21 = res2, cor = cor_vals))
   }
