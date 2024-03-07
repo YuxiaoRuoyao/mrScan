ae6db738c7dbd4bc6f781f75d0084b84bfec89a2
Ruoyao Shi <yuxiaow@umich.edu>
2024-03-06 19:34:01 -0500

    Fix bugs of downstream_filter.R

--- R/downstream_filter.R
+++ R/downstream_filter.R
@@ -4,7 +4,7 @@
 #' @param df_info Dataframe of trait info from previous steps
 #' @param res Bidirection MR results for selected traits
 #' @param sig_level One-sided t-test significant level. Default=0.05
-#' @param method Bidirection MR or MVMR methods. Options: MR_IVW, MR_GRAPPLE,
+#' @param MR_method Bidirection MR or MVMR methods. Options: MR_IVW, MR_GRAPPLE,
 #' MR_ESMR, MR_MRBEE, MVMR_IVW, MVMR_GRAPPLE, MVMR_ESMR, MVMR_MRBEE
 #' @returns A GWAS ID vector, a trait info dataframe, a trait dataframe with four direction
 #' estimate and t-test results
@@ -13,17 +13,17 @@
 #' @import dplyr
 #' @importFrom data.table dcast setDT
 #' @export
-downstream_filter <- function(id_exposure,id.list,df_info,res,sig_level = 0.05,method){
+downstream_filter <- function(id_exposure,id.list,df_info,res,sig_level = 0.05,MR_method){
   df_summary<-data.frame(id=id.list) %>% left_join(df_info[,c("id","trait")],by="id")
   df_YtoZ <- left_join(df_summary,res$mr12,by=c("id"="id.outcome")) %>%
-    filter(method == method) %>%
+    filter(method == MR_method) %>%
     mutate(exposure = if_else(id.exposure == id_exposure, "X", "Y"))
   setDT(df_YtoZ)
   data_wide1 <- dcast(df_YtoZ,id ~ exposure, value.var=c("b","se")) %>%
     select_if(~sum(!is.na(.)) > 0)
   colnames(data_wide1)<-c("id","b_XtoZ","b_YtoZ","se_XtoZ","se_YtoZ")
   df_ZtoY <- left_join(df_summary,res$mr21,by=c("id"="id.exposure")) %>%
-    filter(method == method) %>%
+    filter(method == MR_method) %>%
     mutate(outcome = if_else(id.outcome == id_exposure, "X", "Y"))
   setDT(df_ZtoY)
   data_wide2 <- dcast(df_ZtoY,id ~ outcome, value.var=c("b","se")) %>%
@@ -41,8 +41,8 @@ downstream_filter <- function(id_exposure,id.list,df_info,res,sig_level = 0.05,m
   sig_traits <- union(X_sig,Y_sig)
   downstream_traits <- union(X_downstream,Y_downstream)
   select_trait <- sig_traits[!sig_traits %in% downstream_traits]
-  df_info[df_info$id %in% select_trait,"status"] <- paste0("select after downstream filtering by ",method)
+  df_info[df_info$id %in% select_trait,"status"] <- paste0("select after downstream filtering by ",MR_method)
   filter.trait <- id.list[!id.list %in% select_trait]
-  df_info[df_info$id %in% filter.trait,"status"] <- paste0("delete in downstream filtering by ",method)
+  df_info[df_info$id %in% filter.trait,"status"] <- paste0("delete in downstream filtering by ",MR_method)
   return(list(id.list=select_trait,trait.info=df_info,df_bidirection = df_final))
 }
