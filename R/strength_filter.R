ad15a6fd2e73b943c7dbbe16b25dd3e3a0187609
Ruoyao Shi <yuxiaow@umich.edu>
2024-04-16 22:25:28 -0400

    Update strength_filter.R

--- R/strength_filter.R
+++ R/strength_filter.R
@@ -1,6 +1,10 @@
 #' @title Calculates the conditional F-statistic to assess instrument strength and filter traits
-#' @param dat A data frame of combined GWAS summary data after LD pruning. The required columns include `SNP` for rsID, `trait_ID.beta` for beta hats, `trait_ID.se` for standard errors, `trait_ID.p` for pvalues.
-#' @param R_matrix Pairwise sample overlap matrix among traits
+#' @param dat A data frame of combined GWAS summary data after LD pruning.
+#' For local data, the required columns include `SNP` for rsID, `trait_ID.beta` for beta hats,
+#' `trait_ID.se` for standard errors, `trait_ID.p` for pvalues.
+#' For IEU data, it should be output from TwoSampleMR::mv_harmonise_data().
+#' @param dat_type Either "local" or "IEU". Default  = "local"
+#' @param R_matrix Pairwise sample overlap matrix among traits. Default = NULL
 #' @param df_info Dataframe of trait info from previous step
 #' @param pval_threshold pvalue cutoff for selecting instruments. Default = 5e-8
 #' @param F_threshold F-statistic cutoff. Default = 5
@@ -12,35 +16,50 @@
 #' @import MVMR
 #' @importFrom purrr map_dfr
 #' @export
-strength_filter <- function(dat,R_matrix,df_info,
+strength_filter <- function(dat,dat_type = "local",R_matrix = NULL,df_info,
                             pval_threshold = 5e-8,F_threshold = 5,
                             Filter = FALSE, extra_traits = "None"){
-  snp <- data.frame(dat$snp)
-  beta_hat <- dat %>% select(ends_with(".beta"))
-  se <- dat %>% select(ends_with(".se"))
-  p <- dat %>% select(ends_with(".p"))
-  nms <- stringr::str_replace(names(beta_hat), ".beta", "")
-  names(beta_hat)<-names(se)<-names(p)<-nms
-  o <- match(colnames(R_matrix), nms)
-  beta_hat <- data.frame(beta_hat[, o],check.names = F)
-  se <- data.frame(se[, o],check.names = F)
-  i <- ncol(beta_hat)
-  pmin <- apply(p[,-1, drop = F], 1, min)
-  ix <- which(pmin < pval_threshold)
-  exp_se <- as.matrix(se[ix,2:i])
-  F.data<- format_mvmr(BXGs = as.matrix(beta_hat[ix, 2:i]),
-                       BYG = beta_hat[ix,1],
-                       seBXGs = exp_se,
-                       seBYG = se[ix,1],
-                       RSID = snp[ix,1])
-  sigmalist <- vector("list", length(ix))
-  omega <- R_matrix[2:i,2:i]
-  for (j in 1:length(ix)) {
-    se_matrix <- diag(exp_se[j,],nrow = i-1)
-    sigmalist[[j]] <- se_matrix %*% omega %*% se_matrix
+  if(dat_type == "local"){
+    snp <- data.frame(dat$snp)
+    beta_hat <- dat %>% select(ends_with(".beta"))
+    se <- dat %>% select(ends_with(".se"))
+    p <- dat %>% select(ends_with(".p"))
+    nms <- stringr::str_replace(names(beta_hat), ".beta", "")
+    names(beta_hat)<-names(se)<-names(p)<-nms
+    o <- match(colnames(R_matrix), nms)
+    beta_hat <- data.frame(beta_hat[, o],check.names = F)
+    se <- data.frame(se[, o],check.names = F)
+    i <- ncol(beta_hat)
+    pmin <- apply(p[,-1, drop = F], 1, min)
+    ix <- which(pmin < pval_threshold)
+    exp_se <- as.matrix(se[ix,2:i])
+    F.data<- format_mvmr(BXGs = as.matrix(beta_hat[ix, 2:i]),
+                         BYG = beta_hat[ix,1],
+                         seBXGs = exp_se,
+                         seBYG = se[ix,1],
+                         RSID = snp[ix,1])
+    sigmalist <- vector("list", length(ix))
+    if(!is.null(R_matrix)){
+      omega <- R_matrix[2:i,2:i]
+      for (j in 1:length(ix)) {
+        se_matrix <- diag(exp_se[j,],nrow = i-1)
+        sigmalist[[j]] <- se_matrix %*% omega %*% se_matrix
+      }
+      sres <- data.frame(t(strength_mvmr(r_input = F.data, gencov = sigmalist)))
+    }else{
+      sres <- data.frame(t(strength_mvmr(r_input = F.data, gencov = 0)))
+    }
+    sres$id<-colnames(beta_hat[-1])
   }
-  sres <- data.frame(t(strength_mvmr(r_input = F.data, gencov = sigmalist)))
-  sres$id<-colnames(beta_hat[-1])
+  if(dat_type == "IEU"){
+    F.data<- format_mvmr(BXGs = as.matrix(dat$exposure_beta),
+                         BYG = dat$outcome_beta,
+                         seBXGs = as.matrix(dat$exposure_se),
+                         seBYG = dat$outcome_se,
+                         RSID = rownames(dat$exposure_beta))
+    sres <- data.frame(t(strength_mvmr(r_input = F.data, gencov = 0)))
+    sres$id<-colnames(dat$exposure_beta)
+  }
   if(Filter == TRUE){
     select.id <- sres %>% filter(F.statistic > F_threshold) %>% pull(id)
     if(extra_traits != "None"){
--- pipeline/R/4_strength_filter_initial.R
+++ pipeline/R/4_strength_filter_initial.R
@@ -3,7 +3,6 @@ library(mrScan)
 library(MVMR)
 
 dat_files <- unlist(snakemake@input[["dat"]])
-R_files <- unlist(snakemake@input[["R"]])
 df_info <- read.csv(snakemake@input[["trait_info"]])
 pval_threshold <- as.numeric(snakemake@params[["pval_threshold"]])
 F_threshold <- as.numeric(snakemake@params[["F_threshold"]])
@@ -14,18 +13,18 @@ out_df_strength <- snakemake@output[["out_df_strength"]]
 all_res <- list()
 for (i in seq_along(dat_files)) {
   dat <- readRDS(dat_files[i])
-  R_matrix <- as.matrix(readRDS(R_files[i])$Re)
-  res <- strength_filter(dat = dat, R_matrix = R_matrix, df_info = df_info,
+  res <- strength_filter(dat = dat, dat_type = "IEU",df_info = df_info,
                          pval_threshold = pval_threshold,
                          F_threshold = F_threshold,
                          Filter = FALSE)
-  res <- cbind(res[2,],res[1,])
-  colnames(res)[4:6] <- c("F_main_exp","id_main_exp","trait_main_exp")
-  all_res[[i]] <- res
+  df <- data.frame(t(c(as.matrix(res))))
+  colnames(df) <- paste0(rep(colnames(res), each = nrow(res)), 1:nrow(res))
+  all_res[[i]] <- df
 }
 final_res <- bind_rows(all_res)
 filter_id <- final_res %>%
-  filter(F.statistic > F_threshold & F_main_exp > F_threshold) %>% pull(id)
+  filter(F.statistic1 > F_threshold & F.statistic2 > F_threshold & F.statistic3 > F_threshold) %>%
+  pull(id)
 all_id <- final_res$id
 other.id <- all_id[!all_id %in% filter_id]
 df_info[df_info$id %in% other.id,"status"] <- "Delete due to weak instruments strength"
